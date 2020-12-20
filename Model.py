cce510319c36620105c09a147e3dae2dd7db508e
erfan <erfann.gh1379@gmail.com>
2020-12-20 18:05:08 +0330

    Card class completed.

--- Model.py
+++ Model.py
@@ -8,14 +8,25 @@ class State:
         pass
 
 
-class batch:
+class Batch:
     def __init__(self):
         pass
 
 
 class Card:
-    def __init__(self):
-        pass
+    def __init__(self, color, number):
+        self.color = color
+        self.number = number
+
+    def get_color(self):
+        return self.color
+
+    def get_number(self):
+        return self.number
+
+    def __eq__(self, other):
+        if isinstance(other, Card):
+            return self.number == other.number and self.color == other.color
 
 
 class Input:
