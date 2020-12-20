1cff0fd11de73bea3e5aad6e54b32d7d918d49a9
erfan <erfann.gh1379@gmail.com>
2020-12-20 20:34:55 +0330

    Batch class completed.

--- BFS.py
+++ BFS.py
@@ -1,4 +1,39 @@
 from Model import *
 
 
+test1_card = Card("R", 1)
+test2_card = Card("G", 2)
+test3_card = Card("R", 6)
+test4_card = Card("G", 4)
+test5_card = Card("R", 5)
+test6_card = Card("Y", 9)
+test7_card = Card("R", 7)
+test8_card = Card("B", 8)
+test9_card = Card("Y", 9)
+test10_card = Card("B", 15)
+
+test1_batch = Batch()
+test2_batch = Batch()
+# print(test1_batch.is_empty())
+# print(test10_card.__str__())
+print(test1_batch.sorted_until)
+test1_batch.insert_card(test3_card)
+print(test1_batch.sorted_until)
+test1_batch.insert_card(test2_card)
+print(test1_batch.sorted_until)
+test1_batch.insert_card(test1_card)
+print(test1_batch.sorted_until)
+print(test1_batch.cards_count)
+
+print(test1_batch.is_sorted())
+# test2_batch.insert_card(test1_card)
+# test2_batch.insert_card(test2_card)
+# print(test1_batch.is_empty())
+
+# print(test1_batch.last_card_number())
+# print(test1_batch.last_card_number())
+# print(test1_batch == test2_batch)
+print(test1_batch.__str__())
+
+
 
--- Model.py
+++ Model.py
@@ -1,16 +1,4 @@
-class Search:
-    def __init__(self):
-        pass
-
-
-class State:
-    def __init__(self):
-        pass
-
-
-class Batch:
-    def __init__(self):
-        pass
+from collections import deque
 
 
 class Card:
@@ -28,8 +16,78 @@ class Card:
         if isinstance(other, Card):
             return self.number == other.number and self.color == other.color
 
+    def __str__(self):
+        return str(self.number) + str(self.color)
 
-class Input:
+
+class Batch:
+    def __init__(self):
+        self.cards_count = 0;
+        self.cards = deque()
+        self.sorted_until = 0
+
+    def is_empty(self):
+        return self.cards.__len__() == 0
+
+    def update_sorted_index(self):
+        if self.cards_count == 0:
+            self.sorted_until += 1
+            self.cards_count += 1
+        else:
+            self.cards_count += 1
+            color_match = self.cards.__getitem__(self.cards_count-1).color == self.cards.__getitem__(self.cards_count-2).color
+            number_match = self.cards.__getitem__(self.cards_count-1).number < self.cards.__getitem__(self.cards_count-2).number
+            if number_match and color_match:
+                self.sorted_until += 1
+
+    def insert_card(self, card: Card):
+        self.cards.append(card)
+        self.update_sorted_index()
+
+    def pop_card(self):
+        card = self.cards.pop()
+        self.update_sorted_index()
+        self.cards_count -= 1
+        return card
+
+    def is_sorted(self):
+        return self.cards_count == self.sorted_until
+
+    def last_card_number(self):
+        return self.cards.__getitem__(self.cards_count-1).number
+
+    def __eq__(self, other):
+        try:
+            for i in range(self.cards.__len__()):
+                if self.cards.__getitem__(i) == other.cards.__getitem__(i):
+                    continue
+                else:
+                    return False
+            return True
+        except IndexError:
+            return False
+
+    def __str__(self):
+        if self.cards.__len__() == 0:
+            return "#\n"
+
+        representation = ""
+        for card in self.cards:
+            representation += card.__str__() + " "
+        return representation
+
+
+class State:
+    def __init__(self):
+        pass
+
+
+class Search:
+    def __init__(self):
+        pass
+
+
+class IO:
     def __init__(self):
         pass
 
