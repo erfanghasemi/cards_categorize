9e0561fa5cf719cfeda828fb91b7cec617d52906
erfan <erfann.gh1379@gmail.com>
2020-12-20 16:44:17 +0330

    Main classes initialized.

---
+++ Model.py
@@ -0,0 +1,24 @@
+class Search:
+    def __init__(self):
+        pass
+
+
+class State:
+    def __init__(self):
+        pass
+
+
+class batch:
+    def __init__(self):
+        pass
+
+
+class Card:
+    def __init__(self):
+        pass
+
+
+class Input:
+    def __init__(self):
+        pass
+
